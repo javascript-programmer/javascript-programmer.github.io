d34827fa13a8c27738ec411c8446f1d6d11944a9
javascript-programmer <77402812+javascript-programmer@users.noreply.github.com>
2021-02-03 08:35:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,27 @@ def LemNormalize(text):
 
 
 
+
+
+import pyttsx3
+import PyPDF2
+book = open('oop.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+for num in range(7, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+    
+
+
+
+
+
+
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
